3caac6b83878747285d494e395d43110c12ebe1a
Daria Bondarchuk <dariabondarchuk.work@gmail.com>
2020-04-27 12:56:45 +0300

    Add basic routing and empty pages

--- blog/src/components/navigation_bar/index.js
+++ blog/src/components/navigation_bar/index.js
@@ -1,26 +1,46 @@
 import React, {useState} from 'react';
-import './styles.css'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+import Home from '../../pages/home';
+import About from '../../pages/about';
+import Recipes from '../../pages/recipes';
+
+import './styles.css';
 import menuIcon from './menuIcon.svg';
 
 export default function NavigationBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="navContainer">
-      <div className="navIcon">
-        <a href="#" onClick={() => {
-          setIsOpen(!isOpen);
-        }}>
-          <img className="icon" src={menuIcon} alt="Menu icon"/>
-        </a>
+    <Router>
+      <div className="navContainer">
+        <div className="navIcon">
+          <a href="#" onClick={() => {
+            setIsOpen(!isOpen);
+          }}>
+            <img className="icon" src={menuIcon} alt="Menu icon"/>
+          </a>
+        </div>
+        <div className={['navItems', isOpen?'navItemsOpen':'navItemsHidden'].join(' ')}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/recipes">Recipes</Link>
+          <Link to="/">Cosy home</Link>
+          <Link to="/">Contact</Link>
+        </div>
       </div>
-      <div className={['navItems', isOpen?'navItemsOpen':'navItemsHidden'].join(' ')}>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Recipes</a>
-        <a href="#">Cosy home</a>
-        <a href="#">Contact</a>
-      </div>
-    </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/about">
+          <About/>
+        </Route>
+        <Route exact path="/recipes">
+          <Recipes/>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
---
+++ blog/src/pages/recipes/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Recipes() {
+  return (
+    <p>This is recipes page</p>
+  )
+}
